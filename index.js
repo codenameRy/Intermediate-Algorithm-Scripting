ada3e52bb84ca5d16d8934c1936bac15dc23b480
Ryan Lee <ryanm.lee11@gmail.com>
2020-07-04 03:46:12 +0000

    Challenge - Difference of Two Arrays

--- index.js
+++ index.js
@@ -26,3 +26,40 @@ function sumAll2(arr) {
 }
 
 console.log(sumAll2([5, 10])); //45
+
+//Challenge 2 - Diff Two Arrays
+
+/* Compare two arrays and return a new array with any items only found in one of the two given arrays, but not both. In other words, return the symmetric difference of the two arrays.*/
+
+//Solution 1
+
+function diffArray(arr1, arr2) {
+  return arr1
+    .concat(arr2)
+    .filter(item => !arr1.includes(item) || !arr2.includes(item));
+}
+
+console.log(diffArray([1, 2, 3, 5], [1, 2, 3, 4, 5]));
+
+//Solution 2
+
+function diffArray2(arr1, arr2) {
+  var newArr = [];
+
+  function onlyInFirst(first, second) {
+    // Looping through an array to find elements that don't exist in another array
+    for (var i = 0; i < first.length; i++) {
+      if (second.indexOf(first[i]) === -1) {
+        // Pushing the elements unique to first to newArr
+        newArr.push(first[i]);
+      }
+    }
+  }
+
+  onlyInFirst(arr1, arr2);
+  onlyInFirst(arr2, arr1);
+
+  return newArr;
+}
+
+console.log(diffArray2([1, 2, 3, 5, 6], [1, 2, 3, 4, 5]));
