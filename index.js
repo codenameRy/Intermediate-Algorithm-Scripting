bcaccc65cf17bdfc4689f5ddd2de03a0083e931f
Ryan Lee <ryanm.lee11@gmail.com>
2020-07-10 03:28:52 +0000

    Challenge - DNA Pairing

--- index.js
+++ index.js
@@ -267,3 +267,58 @@ function myReplace2(str, before, after) {
 }
 
 console.log(myReplace2("This has a spellngi error", "spellngi", "spelling"))
+
+//Challenge 7 - DNA Pairing
+
+//Solution 1
+function pairElement(str) {
+  // Return each strand as an array of two elements, the original and the pair.
+  var paired = [];
+
+  // Function to check with strand to pair.
+  var search = function(char) {
+    switch (char) {
+      case "A":
+        paired.push(["A", "T"]);
+        break;
+      case "T":
+        paired.push(["T", "A"]);
+        break;
+      case "C":
+        paired.push(["C", "G"]);
+        break;
+      case "G":
+        paired.push(["G", "C"]);
+        break;
+    }
+  };
+
+  // Loops through the input and pair.
+  for (var i = 0; i < str.length; i++) {
+    search(str[i]);
+  }
+
+  return paired;
+}
+
+// test here
+console.log(pairElement("GCG"));
+
+//Solution 2
+
+function pairElement2(str) {
+  //create object for pair lookup
+  var pairs = {
+    A: "T",
+    T: "A",
+    C: "G",
+    G: "C"
+  };
+  //split string into array of characters
+  var arr = str.split("");
+  //map character to array of character and matching pair
+  return arr.map(x => [x, pairs[x]]);
+}
+
+//test here
+console.log(pairElement2("TTGAG"));
