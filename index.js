2d7f5d789ee7e1c2467ca4dabdfd8f03df49392f
Ryan Lee <ryanm.lee11@gmail.com>
2020-07-10 03:31:57 +0000

    Challenge Missing Letters

--- index.js
+++ index.js
@@ -322,3 +322,29 @@ function pairElement2(str) {
 
 //test here
 console.log(pairElement2("TTGAG"));
+
+//Challenge 8 - Missing Letters
+
+/*Find the missing letter in the passed letter range and return it.
+
+If all letters are present in the range, return undefined.*/
+
+//Solution 1
+
+function fearNotLetter(str) {
+  for (var i = 0; i < str.length; i++) {
+    /* code of current character */
+    var code = str.charCodeAt(i);
+
+    /* if code of current character is not equal to first character + no of iteration
+        hence character has been escaped */
+    if (code !== str.charCodeAt(0) + i) {
+      /* if current character has escaped one character find previous char and return */
+      return String.fromCharCode(code - 1);
+    }
+  }
+  return undefined;
+}
+
+// test here
+fearNotLetter("abce");
